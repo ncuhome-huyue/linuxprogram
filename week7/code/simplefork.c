cc22f47abfca2a11a85c06dd437188302ff1ea3a
huyue <2411070064@qq.com>
2019-10-23 16:52:23 +0800

    week7

--- week7/code/simplefork.c
+++ week7/code/simplefork.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int globa = 4;
 
-int main(void)
+int main (void )
 {
-      pid_t pid;
-      int vari = 5;
-      printf("before fork\n");
-      if((pid = fork()) < 0){
-            printf("fork error\n");
-            exit(0);
-      }else if((pid = fork()) == 0){
-            globa++;
-            vari--;
-            printf("Child changed\n");
-            printf("Child: globa = %d vari = %d\n",globa,vari);
-      }else{
-        printf("Parent did not changed\n");
-            printf("Parent: globa = %d vari = %d\n",globa,vari);
-       exit(0);
-      }
+pid_t pid;
+int vari = 5;
 
- 
+printf ("before fork\n" );
+
+if ((pid = fork()) < 0){
+  printf ("fork error\n");
+  exit (0);
+}else if (pid == 0){
+  globa++ ;
+  vari--;
+  printf("Child changed\n");
+}else
+  printf("Parent did not changde\n");
+
+  printf("globa = %d vari = %d\n",globa,vari);
+  exit(0);
 }
