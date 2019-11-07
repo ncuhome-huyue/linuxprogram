9c1d5390ce0c7544add017a9d53e1ee40250893e
huyue <2411070064@qq.com>
2019-11-07 16:35:12 +0800

    week8

---
+++ week8/verify2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int a;
+static int b=10;
+int main(int argc,char* argv[],char* envp[]){
+    int a=10;
+    char* p=(char*)malloc(sizeof(char));
+    printf("a=%d,p=0x%x\n",a,p);
+    getchar();
+    getchar();
+}
